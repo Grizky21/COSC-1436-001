1c46a11bb477382a40396ada214a433090455280
Glen <Grizky21@yahoo.com>
2026-04-23 10:48:37 -0500

    Add files via upload

---
+++ Calculator/Main.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream> 
+#include <string> 
+
+using namespace std; // I wonder what this thing does???
+
+
+int main()
+{
+
+	int theFirst;
+	int the_second_number; //examples of ints: -234, 0, 85959
+
+	cout << "Enter any number between 0 and 9\n";
+
+	cin >> theFirst;
+
+	cout << "\nNext, enter another number between 0 and 99\n";
+
+	cin >> the_second_number;
+
+	cout << "\nYou entered: " << theFirst <<
+		" and " << the_second_number;
+
+	return 0; // completed successfully
+}
